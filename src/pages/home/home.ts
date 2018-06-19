c310ec70e681d92ab869a30921857e48a0494cdd
Talent Malaba <malabatalent@gmail.com>
2018-06-19 15:27:23 +0800

    Google Maps SDK

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  latitute: number;
+  longitude: number;
 
+  @ViewChild('map') mapRef: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController) { }
+
+  ionViewDidLoad() {
+    console.log(this.mapRef);
+    this.displayMap();
+  }
+
+  displayMap() {
+    const location = new google.maps.LatLng(this.latitute, this.longitude);
+
+    const options = {center: location, zoom: 10};
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options)
   }
 
 }
